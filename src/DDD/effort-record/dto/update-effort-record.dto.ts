52602660264a16beeb44785799e475889f51289a
findreamer <findream@126.com>
2025-01-13 20:37:36 +0800

    切换为admin项目

--- src/DDD/effort-record/dto/update-effort-record.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEffortRecordDto } from './create-effort-record.dto';
-
-export class UpdateEffortRecordDto extends PartialType(CreateEffortRecordDto) {}
--- src/DDD/org-type/org-type.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { OrgTypeService } from './org-type.service';
-
-@Controller('org-type')
-export class OrgTypeController {
-  constructor(private readonly orgTypeService: OrgTypeService) {}
-}
